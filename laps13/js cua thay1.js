6fa04e19f216bd642fb629f4fa2bfc08a0d3751f
NguyenDinhHuong30088 <70881463+NguyenDinhHuong30088@users.noreply.github.com>
2020-11-18 11:40:08 +0700

    BaiThi

---
+++ laps13/js cua thay1.js
@@ -0,0 +1,36 @@
+function gridProduct() {
+    var g_html = "";
+    for (var i = 0; i < products.length; i++) {
+        g_html += "<div class=\"col-md-4\">\n" +
+            "                    <div class=\"item text-center\">\n" +
+            "                        <img class=\"product-image\" src=\"" + products[i].image + "\"/>\n" +
+            "                        <h2 class=\"product-name\">" + products[i].name + "</h2>\n" +
+            "                        <div class=\"star\">\n" +
+            "                            <div class=\"star-box\">\n" +
+            "                                <div class=\"star-rate\" style=\"width: " + (products[i].star / 5 * 100) + "%\"></div>\n" +
+            "                            </div>\n" +
+            "                        </div>\n" +
+            "                        <p class=\"price\">$" + products[i].price + "</p>\n" +
+            "                        <a href=\"#\" class=\"add-to-cart text-uppercase\">Add To Cart</a>\n" +
+            "                    </div>\n" +
+            "                </div>";
+    }
+
+    return g_html;
+}
+    function loadData(){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function (){
+            if(this.readyState == 4 && this.status == 200){
+                var rs = this.responseText; //nhan text/jison tu
+                rs = JSON.parse(rs);//chuyen thanh object jison
+                var products = rs.data;
+                //in HTML ra danh sach
+                 var grid = document.getElementById("grid-products");
+                 grid.innerHTML = gridProduct();
+            }
+        }
+        xhttp.open("GET","https://t2008mhuong.herokuapp.com/laps13/lap%20cua%20thay.html?_ijt=p65nlps32nll5lkkrv05vijnsk");
+        xhttp.send();
+    }
+loadData();
